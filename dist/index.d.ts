ca7f309c7a8f6682569623e93619a7e95ca607f0
Toranosuke Ujike <torabit.humble@gmail.com>
2021-08-27 17:36:47 +0900

    add entry file

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+export * from './entity/models/postModel';
+export * from './entity/models/userModel';
+export * from './entity/models/companyModel';
+export * from './entity/repositoryInterface/index';
+export * from './entity/type';
