8adb852457bbb4929270640ab6c45678ba036b2b
Toranosuke Ujike <torabit.humble@gmail.com>
2021-09-08 16:25:24 +0900

    fix

--- src/entity/repositoryInterface/companyMastRepository.ts
+++ src/entity/repositoryInterface/companyMastRepository.ts
@@ -2,7 +2,7 @@ import { Scalars, Company } from "../type";
 
 export interface ICompanyMastRepository {
     createCompany(company: Company): void;
-    updateCompany(company: Company): Company;
+    updateCompany(company: Company): void;
     deleteCompany(company: Company): void;
 
     fetchCompany(
